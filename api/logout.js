f1c7844a8782f47d050e3b25f727656ccb06f20d
Omex <minerelbot@gmail.com>
2025-06-12 09:31:42 +0530

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,8 +1,10 @@
+// api/logout.js
+
 export default function handler(req, res) {
   res.setHeader("Set-Cookie", [
-    "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT",
-    "user=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    // Clear the discord_user cookie
+    "discord_user=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT",
   ]);
-  res.writeHead(302, { Location: "/" });
+  res.writeHead(302, { Location: "/" }); // Redirect to homepage
   res.end();
 }
